ad440246b2b725c6ae977c91fba7bbfb715a67da
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2015-01-23 22:13:40 -0500

    Add sample config

--- src/map.h
+++ src/map.h
@@ -9,6 +9,7 @@
 // input_device(name, path);
 // exclusive_device(name, path);
 
+//exclusive_device(JOY_0, /dev/input/by-id/usb-Thrustmaster_T.Flight_Hotas_X-event-joystick);
 
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -25,6 +26,8 @@
 //  REL_...
 //  ABS_...
 
+//output_device(JOY_1, ABS_X | ABS_Y | ABS_Z | ABS_RZ | ABS_THROTTLE | ABS_HAT0X | ABS_HAT0Y | JOYSTICK_BUTTONS );
+//output_device(KEY_1, KEYBOARD_KEYS);
 
 
 #endif
@@ -57,6 +60,52 @@
 //  is_rel()
 //  is_abs()
 
+//map(JOY_0,        ABS_X, JOY_1,        ABS_X, value_in);
+//map(JOY_0,        ABS_Y, JOY_1,        ABS_Y, value_in);
+//map(JOY_0,        ABS_Z, JOY_1,        ABS_Z, value_in);
+//map(JOY_0,       ABS_RZ, JOY_1,       ABS_RZ, value_in);
+//map(JOY_0, ABS_THROTTLE, JOY_1, ABS_THROTTLE, value_in);
+//
+//map(JOY_0,  BTN_TRIGGER, JOY_1,             , value_in);
+//map(JOY_0,    BTN_THUMB, JOY_1,             , value_in);
+//map(JOY_0,   BTN_THUMB2, JOY_1,             , value_in);
+//map(JOY_0,      BTN_TOP, JOY_1,             , value_in);
+//map(JOY_0,     BTN_TOP2, JOY_1,             , value_in);
+//map(JOY_0,   BTN_PINKIE, JOY_1,             , value_in);
+//map(JOY_0,     BTN_BASE, JOY_1,             , value_in);
+//map(JOY_0,    BTN_BASE2, JOY_1,        KEY_G, value_in);
+//map(JOY_0,    BTN_BASE3, JOY_1,             , value_in);
+//map(JOY_0,    BTN_BASE4, JOY_1,             , value_in);
+//map(JOY_0,    BTN_BASE5, JOY_1,             , value_in);
+//map(JOY_0,    BTN_BASE6, JOY_1,             , value_in);
+//
+//when(number_in == JOY_0 && event_in == ABS_HAT0X,
+//    static event prev = event(0);
+//    if(value_in)
+//    {
+//        prev = (value_in < 0) ? KEY_KP4 : KEY_KP6;
+//        send_event_mod(JOY_1, KEY_LEFTSHIFT, prev, 1);
+//    }
+//    else
+//    {
+//        send_event_mod(JOY_1, KEY_LEFTSHIFT, prev, 0);
+//        prev = event(0);
+//    }
+//)
+//
+//when(number_in == JOY_0 && event_in == ABS_HAT0Y,
+//    static event prev = event(0);
+//    if(value_in)
+//    {
+//        prev = (value_in < 0) ? KEY_KP2 : KEY_KP8;
+//        send_event_mod(JOY_1, KEY_LEFTSHIFT, prev, 1);
+//    }
+//    else
+//    {
+//        send_event_mod(JOY_1, KEY_LEFTSHIFT, prev, 0);
+//        prev = event(0);
+//    }
+//)
 
 
 #endif
