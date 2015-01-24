2546141acf7fb73fad782ce1129050338374f189
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2015-01-23 23:44:12 -0500

    Add copyright headers

--- src/event.hpp
+++ src/event.hpp
@@ -1,4 +1,11 @@
 ////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2015 Dimitry Ishenko
+// Distributed under the GNU GPL v2. For full terms please visit:
+// http://www.gnu.org/licenses/gpl.html
+//
+// Contact: dimitry (dot) ishenko (at) (gee) mail (dot) com
+
+////////////////////////////////////////////////////////////////////////////////
 #ifndef EVENT_HPP
 #define EVENT_HPP
 
