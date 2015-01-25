c7e74bd69d46d46419adf57fe327236478fc0c59
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2015-01-25 00:03:58 -0500

    Initial Orbiter config

--- src/map.h
+++ src/map.h
@@ -15,7 +15,7 @@
 // The 'exclusive_device' grabs the device in exclusive mode (using EVIOCGRAB
 // ioctl).
 
-//exclusive_device(JOY_0, /dev/input/by-id/usb-Thrustmaster_T.Flight_Hotas_X-event-joystick);
+exclusive_device(JOY_0, /dev/input/by-id/usb-Thrustmaster_T.Flight_Hotas_X-event-joystick);
 
 ////////////////////////////////////////////////////////////////////////////////
 // Define output devices using the following macros:
@@ -37,8 +37,8 @@
 //  REL_...             ( see event_gen.hpp )
 //  ABS_...             ( see event_gen.hpp )
 
-//output_device(JOY_1, ABS_X | ABS_Y | ABS_Z | ABS_RZ | ABS_THROTTLE | ABS_HAT0X | ABS_HAT0Y | JOYSTICK_BUTTONS );
-//output_device(KBD_1, KEYBOARD_KEYS);
+output_device(JOY_1, ABS_X | ABS_Y | ABS_Z | ABS_RZ | ABS_THROTTLE | ABS_HAT0X | ABS_HAT0Y | JOYSTICK_BUTTONS );
+output_device(KBD_1, KEYBOARD_KEYS);
 
 #endif
 
@@ -94,53 +94,53 @@
 //  is_rel()
 //  is_abs()
 
-//map(JOY_0,        ABS_X, JOY_1,        ABS_X, value_in);
-//map(JOY_0,        ABS_Y, JOY_1,        ABS_Y, value_in);
-//map(JOY_0,        ABS_Z, JOY_1,        ABS_Z, value_in);
-//map(JOY_0,       ABS_RZ, JOY_1,       ABS_RZ, value_in);
-//map(JOY_0, ABS_THROTTLE, JOY_1, ABS_THROTTLE, value_in);
-//
-//map(JOY_0,  BTN_TRIGGER, JOY_1,  BTN_TRIGGER, value_in);
-//map(JOY_0,    BTN_THUMB, JOY_1,    BTN_THUMB, value_in);
-//map(JOY_0,   BTN_THUMB2, JOY_1,   BTN_THUMB2, value_in);
-//map(JOY_0,      BTN_TOP, JOY_1,      BTN_TOP, value_in);
-//map(JOY_0,     BTN_TOP2, JOY_1,     BTN_TOP2, value_in);
-//map(JOY_0,   BTN_PINKIE, JOY_1,   BTN_PINKIE, value_in);
-//map(JOY_0,     BTN_BASE, JOY_1,     BTN_BASE, value_in);
-//
-//map(JOY_0,    BTN_BASE2, KBD_1,        KEY_G, value_in);
-//map(JOY_0,    BTN_BASE3, KBD_1,      KEY_KP9, value_in, KEY_LEFTALT);
-//map(JOY_0,    BTN_BASE4, KBD_1,      KEY_KP3, value_in, KEY_LEFTALT);
-//
-//map(JOY_0,    BTN_BASE5, JOY_1,    BTN_BASE5, value_in);
-//map(JOY_0,    BTN_BASE6, JOY_1,    BTN_BASE6, value_in);
-//
-//when(name_in == JOY_0 && event_in == ABS_HAT0X,
-//    static auto prev = EVT_NONE;
-//    if(value_in)
-//    {
-//        prev = (value_in < 0) ? KEY_KP4 : KEY_KP6;
-//        send_event(KBD_1, prev, 1, KEY_LEFTALT);
-//    }
-//    else
-//    {
-//        send_event(KBD_1, prev, 0, KEY_LEFTALT);
-//        prev = EVT_NONE;
-//    }
-//);
-//
-//when(name_in == JOY_0 && event_in == ABS_HAT0Y,
-//    static auto prev = EVT_NONE;
-//    if(value_in)
-//    {
-//        prev = (value_in < 0) ? KEY_KP8 : KEY_KP2;
-//        send_event(KBD_1, prev, 1, KEY_LEFTALT);
-//    }
-//    else
-//    {
-//        send_event(KBD_1, prev, 0, KEY_LEFTALT);
-//        prev = EVT_NONE;
-//    }
-//);
+map(JOY_0,        ABS_X, JOY_1,        ABS_X, value_in);
+map(JOY_0,        ABS_Y, JOY_1,        ABS_Y, value_in);
+map(JOY_0,        ABS_Z, JOY_1,        ABS_Z, value_in);
+map(JOY_0,       ABS_RZ, JOY_1,       ABS_RZ, value_in);
+map(JOY_0, ABS_THROTTLE, JOY_1, ABS_THROTTLE, value_in);
+
+map(JOY_0,  BTN_TRIGGER, KBD_1,        KEY_K, value_in);
+map(JOY_0,    BTN_THUMB, KBD_1,        KEY_D, value_in, KEY_LEFTCTRL);
+map(JOY_0,   BTN_THUMB2, JOY_1,   BTN_THUMB2, value_in);
+map(JOY_0,      BTN_TOP, KBD_1,      KEY_KP5, value_in);
+map(JOY_0,     BTN_TOP2, KBD_1,      KEY_KP0, value_in);
+map(JOY_0,   BTN_PINKIE, KBD_1,    KEY_KPDOT, value_in);
+map(JOY_0,     BTN_BASE, KBD_1,  KEY_KPMINUS, value_in);
+
+map(JOY_0,    BTN_BASE2, KBD_1,        KEY_G, value_in);
+map(JOY_0,    BTN_BASE3, KBD_1,      KEY_KP9, value_in, KEY_LEFTALT);
+map(JOY_0,    BTN_BASE4, KBD_1,      KEY_KP3, value_in, KEY_LEFTALT);
+
+map(JOY_0,    BTN_BASE5, KBD_1,        KEY_A, value_in);
+map(JOY_0,    BTN_BASE6, KBD_1,        KEY_L, value_in);
+
+when(name_in == JOY_0 && event_in == ABS_HAT0X,
+    static auto prev = EVT_NONE;
+    if(value_in)
+    {
+        prev = (value_in < 0) ? KEY_KP4 : KEY_KP6;
+        send_event(KBD_1, prev, 1, KEY_LEFTALT);
+    }
+    else
+    {
+        send_event(KBD_1, prev, 0, KEY_LEFTALT);
+        prev = EVT_NONE;
+    }
+);
+
+when(name_in == JOY_0 && event_in == ABS_HAT0Y,
+    static auto prev = EVT_NONE;
+    if(value_in)
+    {
+        prev = (value_in < 0) ? KEY_KP8 : KEY_KP2;
+        send_event(KBD_1, prev, 1, KEY_LEFTALT);
+    }
+    else
+    {
+        send_event(KBD_1, prev, 0, KEY_LEFTALT);
+        prev = EVT_NONE;
+    }
+);
 
 #endif
