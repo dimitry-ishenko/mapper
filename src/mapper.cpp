1ea3530c2a4ef797d3ca219d4953f51219358a11
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2015-02-07 01:58:51 -0500

    version 0.5.1

--- src/mapper.cpp
+++ src/mapper.cpp
@@ -27,7 +27,7 @@ using namespace app;
 ////////////////////////////////////////////////////////////////////////////////
 #define VERSION_MAJOR 0
 #define VERSION_MINOR 5
-#define VERSION_MICRO 0
+#define VERSION_MICRO 1
 
 constexpr char _n = '\n';
 
